951671f3018bccdc8904efd23ff6281d6fb3063d
Bazingiu <giulita00@gmail.com>
2022-05-10 16:57:41 +0200

    Prende gli ultimi tot messaggi e ne scrive il contenuto su un fil

--- sloth_reader.py
+++ sloth_reader.py
@@ -1,37 +1,42 @@
-import discord
+# bot.py
 import os
+import random
+import datetime
+from discord.ext import commands
+import json
 from dotenv import load_dotenv
+import re
 
-# GETS THE BOT (client) OBJECT FROM DISCORD.PY
-bot = discord.Client()
-
-# EVENT LISTENER FOR WHEN THE BOT HAS SWITCHED FROM OFFLINE TO ONLINE
-@bot.event
-async def on_ready():
-	# CREATES A COUNTER TO KEEP TRACK OF HOW MANY GUILDS / SERVERS THE BOT IS CONNECTED TO
-	guild_count = 0
-
-	# LOOPS THROUGH ALL THE GUILD / SERVERS THAT THE BOT IS ASSOCIATED WITH
-	for guild in bot.guilds:
-		# PRINT THE SERVER'S ID AND NAME.
-		print(f"- {guild.id} (name: {guild.name})")
-
-		# INCREMENTS THE GUILD COUNTER.
-		guild_count = guild_count + 1
-
-	# PRINTS HOW MANY GUILDS / SERVERS THE BOT IS IN
-	print("SampleDiscordBot is in " + str(guild_count) + " guilds.")
-
-# EVENT LISTENER FOR WHEN A NEW MESSAGE IS SENT TO A CHANNEL
-@bot.event
-async def on_message(message):
-	# CHECKS IF THE MESSAGE THAT WAS SENT IS EQUAL TO "HELL".
-	if message.content == "cacca":
-		# SENDS BACK A MESSAGE TO THE CHANNEL.
-		await message.channel.send("Ciao fra")
-
-# Loads the .env file that resides on the same level as the script
 load_dotenv()
-# Grab the API token from the .env file
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-bot.run(DISCORD_TOKEN)
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.command(name = 'riassumimi')
+async def phrase(ctx, message_number):
+    #Controlla se è un intero
+    if re.match(r'^\s*\d+\s*$', message_number):
+        message_number = int(message_number)
+        messages = await ctx.channel.history(limit=message_number, oldest_first=False).flatten()
+     
+        file = open('messages.txt', 'w')
+        for element in messages:
+            # Prendo i dati che mi servono
+            channel = element.channel
+            author = element.author
+            content = element.content
+
+            print(channel, author, content)
+            print(type(channel), type(author), type(content))
+
+            file.write(content + "\n")    
+        file.close()
+    else:
+        await ctx.send("METTI UN INTERO CRETINO")
+
+# @bot.event
+# async def on_message(message):
+# 	if message.content == "qual'e la risposta?":
+# 		await message.channel.send("42")
+
+bot.run("OTcyMTM1NTcwMzMzNjMwNTE0.Gc7EPP.wi28PC2ryWo5WYCoGAUwg0HgMv0pEYzi6VH3j0")
